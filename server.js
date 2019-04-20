597255cd4b0ec4a228ce04d965ee829ecedc982a
mloweedgar <mloweedgar0@gmail.com>
2019-04-20 14:16:03 +0300

    feature: add users and national parks routes

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ MongoClient.connect(url, (err, db) => {
   // this is http server
   // it serves contents from the databases
   http
-    .createServer(function(req, res) {
+    .createServer(function (req, res) {
       // setting up headers
-      res.writeHead(200, { "Content-Type": "application/json" , 'Access-Control-Allow-Origin': '*'});
-      dbo.collection('nationalParks').find({}).toArray(function(err, results) {
-        res.end(JSON.stringify(results));
-      });
+      res.writeHead(200, { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*' });
+
+      // users route
+      if (req.url === '/api/v1/users') {
+        // gets data from the database
+        dbo.collection('users').find({}).toArray(function (err, users) {
+          // returning users as our response
+          res.end(JSON.stringify(users));
+        });
+      }
+
+      // national parks 
+      else if (req.url === '/api/v1/national-parks') {
+        // get the national parks from the database
+        dbo.collection('nationalParks').find({}).toArray(function (err, nationalparks) {
+          // returning national parks  as our response
+          res.end(JSON.stringify(nationalparks));
+        });
+      }
+      else if (req.url === '/') {
+        res.end(' You are home');
+      }
+      else {
+        res.end('not found');
+      }
     })
     .listen(8080, () => {
       console.log("server is up: open localhost:8080");
